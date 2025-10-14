daab48d0f7fe25e190598e70aa041f7a08d83c5a
songduckhee <vhaqjvl1@gmail.com>
2025-10-14 19:33:09 +0900

    feat. 랜덤하게 몬스터가 나타나는걸 구현

--- TEAMPROJECT_TEXTRPG/battleStart.cs
+++ TEAMPROJECT_TEXTRPG/battleStart.cs
@@ -6,35 +6,93 @@ using System.Threading.Tasks;
 
 namespace TEAMPROJECT_TEXTRPG
 {
-    internal class battleStart
+    public class battleStart
     {
 
 
 
 
-        List<Monster> monster = new List<Monster>()
+        List<Monster> monster = new List<Monster>() {
+
+            new Monster("미니언", 2, 15, 5),
+            new Monster("공허충", 3, 10, 9),
+            new Monster("대포미니언", 5, 25, 8)
+
+
+
+        };
+
+
+        public void StartBattle()
+        {
+
+            Console.WriteLine("Battle!!");
+            Console.WriteLine();
+
+            Random random = new Random();
+
+            int monsterCount = random.Next(0, 2); ;
+
+            Monster spawn = monster[random.Next(monster.Count)];
+
+            List<Monster> spawnMon = new List<Monster>() { spawn };
+
+            for (int i = 0; i < monsterCount; i++)
+
             {
+                Console.WriteLine($"Lv.{spawnMon[i].Level} {spawnMon[i].Name} HP {spawnMon[i].Hp}");
 
-                new Monster("미니언",2,15,5 ),new Monster("공허충",3,10,9 ),
-                new Monster("대포미니언",5,25,8 )
+            }
 
 
-            };
 
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("[내정보]");
+            Console.WriteLine("Lv.1  Chad (전사)");
+            Console.WriteLine("HP 100/100");
+            Console.WriteLine();
+            Console.WriteLine("1. 공격");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
+            string input = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
 
 
         public class Monster
         {
 
 
-            public string Name { get; set; }
+            
 
-            public int Level { get; set; }
+            public string Name;
 
-            public int Hp { get; set; }
+            public int Level;
+
+            public int Hp;
 
             public int Atk { get; set; }
-
+        
 
 
 
@@ -44,24 +102,12 @@ namespace TEAMPROJECT_TEXTRPG
                 Level = level;
                 Hp = hp;
                 Atk = atk;
+                
             }
 
 
-            public void StartBattle()
-            {
 
-                Console.WriteLine("Battle!!");
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-
-
-
-
-
-
-
-            }
+           
 
 
 
@@ -83,5 +129,7 @@ namespace TEAMPROJECT_TEXTRPG
 
 
 
-    }
+
+
+    
 }
