b656b47ca9fd3f61776c46e1fdf8973fbba610b7
kimeunseo <kimeunseo.work@gmail.com>
2025-10-14 14:08:09 +0900

    .

--- TEAMPROJECT_TEXTRPG/Program.cs
+++ TEAMPROJECT_TEXTRPG/Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+<<<<<<< Updated upstream
       
+=======
+            GameManager.Instance.Run();
+>>>>>>> Stashed changes
         }
     }
 }
