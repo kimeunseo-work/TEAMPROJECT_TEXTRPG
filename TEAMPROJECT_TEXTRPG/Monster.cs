b9d5e20d69dfe81a022d0de59ed81152038ce4ed
songduckhee <vhaqjvl1@gmail.com>
2025-10-14 20:12:26 +0900

    feat.Monster Script 추가
    script 추가 몬스터 정보 분리

---
+++ TEAMPROJECT_TEXTRPG/Monster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAMPROJECT_TEXTRPG
+{
+    internal class Monster
+    {
+        public string Name;
+
+        public int Level;
+
+        public int Hp;
+
+        public int Atk { get; set; }
+
+
+
+
+        public Monster(string name, int level, int hp, int atk)
+        {
+            Name = name;
+            Level = level;
+            Hp = hp;
+            Atk = atk;
+
+        }
+
+    }
+
+
+    internal class Monsters
+    {
+
+
+       internal List<Monster> monster = new List<Monster>() {
+
+            new Monster("미니언", 2, 15, 5),
+            new Monster("공허충", 3, 10, 9),
+            new Monster("대포미니언", 5, 25, 8)
+
+
+
+        };
+
+
+
+
+
+
+    }
+
+
+}
