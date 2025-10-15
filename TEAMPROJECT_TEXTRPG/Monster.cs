d3fb2411ab9d071f98122e8e6856e769437d47e9
kimeunseo <kimeunseo.work@gmail.com>
2025-10-15 10:48:33 +0900

    feat. 테스트 위한 몬스터 랜덤 소환 기능 구현

--- TEAMPROJECT_TEXTRPG/Monster.cs
+++ TEAMPROJECT_TEXTRPG/Monster.cs
@@ -1,56 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
 
 namespace TEAMPROJECT_TEXTRPG
 {
     internal class Monster
     {
-        public string Name;
 
-        public int Level;
-
-        public int Hp;
-
-        public int Atk { get; set; }
-
-
-
-
-        public Monster(string name, int level, int hp, int atk)
         {
             Name = name;
             Level = level;
             Hp = hp;
             Atk = atk;
-
         }
 
     }
 
-
     internal class Monsters
     {
-
-
-       internal List<Monster> monster = new List<Monster>() {
-
             new Monster("미니언", 2, 15, 5),
             new Monster("공허충", 3, 10, 9),
             new Monster("대포미니언", 5, 25, 8)
-
-
-
         };
 
-
-
-
-
-
+        /// <summary>
+        /// 몬스터 소환 메서드
+        /// </summary>
+        internal List<Monster> SpawnRandomMonsters()
+        {
+            var monsterCount = new Random().Next(1, 5);
+            var monsters = GetRandomMonsters(monsterCount);
+            
+            return monsters;
+        }
+        
+        private List<Monster> GetRandomMonsters(int count)
+        {
+            var monsters = new List<Monster>();
+            var monsterCount = monster.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var index = new Random().Next(monsterCount);
+                monsters.Add(monster[index]);
+            }
+            return monsters;
+        }
     }
-
-
 }
