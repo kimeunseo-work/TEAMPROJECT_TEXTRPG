08a3bfd08394edc3889d4afc6d52261a9ea805fc
Hagill <youmu12589@gmail.com>
2025-10-15 17:06:57 +0900

    feat. 전투 중 사용자 input

--- TEAMPROJECT_TEXTRPG/InputHandler.cs
+++ TEAMPROJECT_TEXTRPG/InputHandler.cs
@@ -26,5 +26,29 @@ namespace TEAMPROJECT_TEXTRPG
                 return int.MaxValue;
             }
         }
+
+
+        public static int GetUserActionInputInBattle()
+        {
+            Console.WriteLine();
+            Console.WriteLine("대상을 선택해주세요.");
+            Console.Write(">> ");
+            string input = Console.ReadLine() ?? "";    // null을 반환할 경우 빈 문자열을 대신 사용
+            string cleanInput = Regex.Replace(input, @"\s+", "");   // 입력받은 문자열에서 공백 제거
+
+            int parsedInput;
+            bool parsedSuccess = int.TryParse(cleanInput, out parsedInput);
+
+            if (parsedSuccess)
+            {
+                return parsedInput;
+            }
+            else
+            {
+                // 숫자가 아닌 입력이나 유효하지 않은 입력은 int.MaxValue로 반환하여 씬에서 처리
+                return int.MaxValue;
+            }
+        }
+
     }
 }
