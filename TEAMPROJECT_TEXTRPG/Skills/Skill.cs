70912a08fb81488c0e9f4634e5d0934239a70435
rwy685 <rwy685@gmail.com>
2025-10-16 16:46:50 +0900

    feat.SkillAction 메서드 구현

--- TEAMPROJECT_TEXTRPG/Skills/Skill.cs
+++ TEAMPROJECT_TEXTRPG/Skills/Skill.cs
@@ -4,26 +4,41 @@
     {
         public string Name {  get; set; }
         public int Mp {  get; set; }
-        public int Damage {  get; set; }
-        public int Multiple {  get; set; }
+        public double Multiple {  get; set; }
         public bool IsRandom {  get; set; }
         public int Count {  get; set; }
         public string Description {  get; set; }
-    }
+    }   
+        //스킬 데이터베이스 (싱글톤)
         internal class SkillDatabase
     {
+        //유일한 저장 공간
+        private static SkillDatabase instance;
+
+        //외부에서 겁근 가능
+
+        internal static SkillDatabase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SkillDatabase(); // 처음에만 생성
+                }
+                return instance;
+            }
+        }
+
+        //전사 스킬 종류
         internal List<Skill> WarriorSkills = new();
 
         public SkillDatabase()
         {
-            var atk = (int)CharacterManager.Instance.player.Attack;
-
             WarriorSkills.Add(new Skill
             {
                 Name = "알파 스트라이크",
                 Mp = 10,
-                Damage = atk * 2,
-                Multiple = 1,
+                Multiple = 2,
                 IsRandom = false,
                 Count = 1,
                 Description = "하나의 적을 공격합니다."
@@ -33,8 +48,7 @@
             {
                 Name = "더블 스트라이크",
                 Mp = 10,
-                Damage = (int)(atk * 1.5f),
-                Multiple = 2,
+                Multiple = 1.5,
                 IsRandom = true,
                 Count = 2,
                 Description = "2명의 적을 랜덤으로 공격합니다."
