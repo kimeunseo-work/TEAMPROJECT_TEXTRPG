f05a894d43be5dca645cfc22270de3689dc14577
rwy685 <rwy685@gmail.com>
2025-10-16 12:30:16 +0900

    feat.Skill 구조체 및 SkillAction 클래스 생성

---
+++ TEAMPROJECT_TEXTRPG/Skill.cs
@@ -0,0 +1,27 @@
+﻿namespace TEAMPROJECT_TEXTRPG
+{
+    internal struct Skill
+    {
+        public string Name { get; }
+        public int Mp { get; }
+
+        public int Damage { get; }
+        public int Multiple { get; }
+        public bool IsRandom { get; }
+        public int Count { get; }
+        public string Description { get; }
+
+        public Skill(string name, int mp, int damage, int multiple, bool israndom, int count, string description)
+        {
+            Name = name;
+            Mp = mp;
+            Damage = damage;
+            Multiple = multiple;
+            IsRandom = israndom;
+            Count = count;
+            Description = description;
+
+        }
+    }
+
+}
