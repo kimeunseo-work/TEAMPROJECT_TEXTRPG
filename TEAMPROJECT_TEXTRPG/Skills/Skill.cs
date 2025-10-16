a832b0c540719eeba37148767af08256aaa7f9b6
rwy685 <rwy685@gmail.com>
2025-10-16 15:12:23 +0900

    fix.SkillDatabase.cs파일 삭제 및 Skill 구조체 생성자 제거

--- TEAMPROJECT_TEXTRPG/Skills/Skill.cs
+++ TEAMPROJECT_TEXTRPG/Skills/Skill.cs
@@ -2,26 +2,49 @@
 {
     internal struct Skill
     {
-        public string Name { get; }
-        public int Mp { get; }
-
-        public int Damage { get; }
-        public int Multiple { get; }
-        public bool IsRandom { get; }
-        public int Count { get; }
-        public string Description { get; }
-
-        public Skill(string name, int mp, int damage, int multiple, bool israndom, int count, string description)
-        {
-            Name = name;
-            Mp = mp;
-            Damage = damage;
-            Multiple = multiple;
-            IsRandom = israndom;
-            Count = count;
-            Description = description;
-
-        }
+        public string Name {  get; set; }
+        public int Mp {  get; set; }
+        public int Damage {  get; set; }
+        public int Multiple {  get; set; }
+        public bool IsRandom {  get; set; }
+        public int Count {  get; set; }
+        public string Description {  get; set; }
     }
+        internal class SkillDatabase
+    {
+        internal List<Skill> WarriorSkills = new();
+
+        public SkillDatabase()
+        {
+            var atk = (int)CharacterManager.Instance.player.Attack;
+
+            WarriorSkills.Add(new Skill
+            {
+                Name = "알파 스트라이크",
+                Mp = 10,
+                Damage = atk * 2,
+                Multiple = 1,
+                IsRandom = false,
+                Count = 1,
+                Description = "하나의 적을 공격합니다."
+            });
+
+            WarriorSkills.Add(new Skill()
+            {
+                Name = "더블 스트라이크",
+                Mp = 10,
+                Damage = (int)(atk * 1.5f),
+                Multiple = 2,
+                IsRandom = true,
+                Count = 2,
+                Description = "2명의 적을 랜덤으로 공격합니다."
+            });
+        }
+
+    }
+
+    
+    
+
 
 }
--- TEAMPROJECT_TEXTRPG/Skills/SkillAction.cs
+++ TEAMPROJECT_TEXTRPG/Skills/SkillAction.cs
@@ -10,15 +10,31 @@ namespace TEAMPROJECT_TEXTRPG.Skills
     {
         internal static void Action(Player player, Monster monster, Skill skill)
         {
+            //MP 충분한지 확인
             if (player.Mp < skill.Mp)
             {
                 Console.WriteLine($"{player.Name}의 MP가 부족합니다.");
                 return;
             }
 
+            //MP 차감
             player.Mp -= skill.Mp;
 
+            //스킬 실행 시 문구 출력
+            Console.WriteLine($"{player.Name}이(가) {skill.Name}을 사용했습니다!");
+            Console.WriteLine($">>{skill.Description}");
 
+            //누구에게 지정할지
+            if (!skill.IsRandom)
+            {
+                var target = GameManager.Instance.monsters[0];
+                Console.WriteLine($"{target.Name}을(를) 지정했습니다");
+            }
+            else
+            {
+                Console.WriteLine($"{skill.Count}명의 랜덤 적을 대상으로 지정했습니다.");
+            }
+            
         }
     }
 }
