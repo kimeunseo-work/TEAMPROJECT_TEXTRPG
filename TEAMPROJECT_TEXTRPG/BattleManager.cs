8a787936f343de0f19427df95a6a007c5ce4fabe
kimeunseo <kimeunseo.work@gmail.com>
2025-10-17 12:41:29 +0900

    feat: 배틀매니저 싱글톤 구현

---
+++ TEAMPROJECT_TEXTRPG/BattleManager.cs
@@ -0,0 +1,36 @@
+﻿using TEAMPROJECT_TEXTRPG.Scenes;
+
+namespace TEAMPROJECT_TEXTRPG
+{
+    /// <summary>
+    /// 배틀 결과 상태
+    /// </summary>
+    internal enum NewBattleState
+    {
+        None,// 전투 X
+        Start,// 전투 시작. 몬스터 소환
+        PlayerTurn,
+        MonsterTurn,
+        Victory,
+        Lose
+    }
+
+    internal class BattleManager
+    {
+        /* 싱글톤 */
+        //============================================================//
+        private static BattleManager instance;
+        internal static BattleManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BattleManager();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
