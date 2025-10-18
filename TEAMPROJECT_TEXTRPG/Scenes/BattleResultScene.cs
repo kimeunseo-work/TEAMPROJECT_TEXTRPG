ccbec4435ad45d30ccef638ce887ae40eeed9aa8
JeonGyuTae <115717251+JeonGyuTae@users.noreply.github.com>
2025-10-18 17:09:47 +0900

    fix. 랜덤 몬스터 처치 시 획득하는 경험치 합이 초기화되지 않는 오류 수정

--- TEAMPROJECT_TEXTRPG/Scenes/BattleResultScene.cs
+++ TEAMPROJECT_TEXTRPG/Scenes/BattleResultScene.cs
@@ -3,13 +3,16 @@
     internal class BattleResultScene : Scene
     {
         string input;           // Player 의 입력을 받아올 값에 대한 선언 (= 입력 좀 해줄래?)
-        int subExp = 0;         // 랜덤으로 생성한 몬스터들의 경험치의 합 연산 값을 정의하기 위한 선언 (= 경험치 {subExp} 획득!)
+        int subExp;         // 랜덤으로 생성한 몬스터들의 경험치의 합 연산 값을 정의하기 위한 선언 (= 경험치 {subExp} 획득!)
         int leveled;            // 레벨업 전 레벨 값 정의를 위한 선언 (= 레벨_이었던 것)
         int leveling;           // 레벨업 후 레벨 값 정의를 위한 선언 (= 지금 레벨인 것)
 
         // 전투 결과창(승리)   ==>>   전투에서 승리했을 경우 출력하는 씬
         internal void BattleResultWin()
         {
+            // 랜덤 몬스터 경험치 합 초기화
+            subExp = 0;
+
             // 랜덤 몬스터 경험치 합 연산 (반복문)
             for (int i = 0; i < GameManager.Instance.monsters.Count; i++)
             {
