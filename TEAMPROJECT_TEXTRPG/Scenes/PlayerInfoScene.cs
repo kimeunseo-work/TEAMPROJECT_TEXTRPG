7d53863f92294dda53dcd5bdab78a61258ec5f4c
JeonGyuTae <115717251+JeonGyuTae@users.noreply.github.com>
2025-10-20 21:53:26 +0900

    feat. 인벤토리 출력
    출력 테스트를 위한 임시 작업(?)

--- TEAMPROJECT_TEXTRPG/Scenes/PlayerInfoScene.cs
+++ TEAMPROJECT_TEXTRPG/Scenes/PlayerInfoScene.cs
@@ -1,4 +1,5 @@
-﻿using TEAMPROJECT_TEXTRPG.Managers;
+﻿using TEAMPROJECT_TEXTRPG.Core;
+using TEAMPROJECT_TEXTRPG.Managers;
 using TEAMPROJECT_TEXTRPG.Utility;
 
 namespace TEAMPROJECT_TEXTRPG.Scenes
@@ -19,6 +20,13 @@ namespace TEAMPROJECT_TEXTRPG.Scenes
                 Console.Clear();
                 Console.WriteLine($"\nLv.{CharacterManager.Instance.player.Level}\n\n{CharacterManager.Instance.player.Name}({CharacterManager.Instance.player.CurrentJob.Name})\n\n공격력: {CharacterManager.Instance.player.Attack:F1}\n\n방어력: {CharacterManager.Instance.player.Defense}" +
                 $"\n\n체력: {CharacterManager.Instance.player.Hp}\n\n마나: {CharacterManager.Instance.player.Mp}\n\n경험치: {CharacterManager.Instance.player.Exp}\n\nGold: {CharacterManager.Instance.player.Gold}");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("[ 보유 아이템 목록 ]");
+                Console.WriteLine();
+                CharacterManager.Instance.player.ShowInventory();
+                Console.WriteLine();
 
                 Console.WriteLine($"\n0. 나가기");
 
